bfef110d0854339c46810c8ac7f2902f65d9f313
Shinnosuke Sawada <sawada@nlp.ist.i.kyoto-u.ac.jp>
2017-12-01 18:17:32 +0900

    add

---
+++ set.go
@@ -0,0 +1,52 @@
+package set
+
+type S map[string]struct{}
+
+func New(items ...string) *S {
+	s := make(S)
+	for _, item := range items {
+		s[item] = struct{}{}
+	}
+	return &s
+}
+
+func (s *S) Add(i string) {
+	(*s)[i] = struct{}{}
+}
+
+func (s *S) Update(u *S) {
+	for i := range *u {
+		s.Add(i)
+	}
+}
+
+func And(a, b *S) *S {
+	s := New()
+	for i := range *a {
+		if _, ok := (*b)[i]; ok {
+			s.Add(i)
+		}
+	}
+	return s
+}
+
+func Not(a, b *S) *S {
+	s := New()
+	for i := range *a {
+		if _, ok := (*b)[i]; !ok {
+			s.Add(i)
+		}
+	}
+	return s
+}
+
+func Or(a, b *S) *S {
+	s := New()
+	s.Update(a)
+	s.Update(b)
+	return s
+}
+
+func Xor(a, b *S) *S {
+	return Or(Not(a, b), Not(b, a))
+}
